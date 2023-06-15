7e4a2c49997b4e0ad031c7c17d02d962991b467e
999mattia <mattia.gisiger@gmail.com>
2023-06-15 10:24:20 +0200

    buttons>links

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -1,16 +1,22 @@
 import { deletePost } from "../utils/fetch";
+import { useNavigate } from "react-router-dom";
 
 export default function PostCard(props: any) {
-	let detailLink = "/posts/" + props.post.id;
-	let editLink = "/posts/" + props.post.id + "/edit";
+	const navigate = useNavigate();
 	return (
 		<div>
 			<div>
 				<h5>{props.post.title}</h5>
 				<h6>{props.post.author}</h6>
 				<p>{props.post.content}</p>
-				<a href={detailLink}>Read more</a>
-				<a href={editLink}>Edit</a>
+				<button onClick={() => navigate(`/posts/${props.post.id}`)}>
+					Read more
+				</button>
+				<button
+					onClick={() => navigate(`/posts/${props.post.id}/edit`)}
+				>
+					Edit
+				</button>
 				<button
 					onClick={async () => {
 						await deletePost(props.post.id);
