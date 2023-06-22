250cf96b3c8babf5411a99b9dce6168ec6bcaa21
999mattia <mattia.gisiger@gmail.com>
2023-06-22 08:39:05 +0200

    updated types in frontend

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,13 +1,15 @@
 export interface Comment {
-    id: string;
-    text: string;
-    author: string;
+	id: string;
+	text: string;
+	author: string;
+	additional?: string;
 }
 
 export interface Post {
-    id: string;
-    title: string;
-    content: string;
-    author: string;
-    comments: Comment[];
+	id: string;
+	title: string;
+	content: string;
+	author: string;
+	comments: Comment[];
+	additional?: string;
 }
